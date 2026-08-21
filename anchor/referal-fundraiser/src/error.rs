7f16585d2914be39e36d1edb4ef7a20251824608
Rox <11506193+MonikerMan@users.noreply.github.com>
2026-08-21 20:52:43 +0200

    Add FundraiserError enum for error handling

--- anchor/referal-fundraiser/src/error.rs
+++ anchor/referal-fundraiser/src/error.rs
@@ -1,1 +1,29 @@
+use anchor_lang::error_code;
 
+#[error_code]
+pub enum FundraiserError {
+    #[msg("The fundraiser is currently paused.")]
+    ProgramPaused,
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+    #[msg("The amount to raise has not been met")]
+    TargetNotMet,
+    #[msg("The amount to raise has been achieved")]
+    TargetMet,
+    #[msg("The contribution is too big")]
+    ContributionTooBig,
+    #[msg("The contribution is too small")]
+    ContributionTooSmall,
+    #[msg("The maximum amount to contribute has been reached")]
+    MaximumContributionsReached,
+    #[msg("The fundraiser has not ended yet")]
+    FundraiserNotEnded,
+    #[msg("The fundraiser has ended")]
+    FundraiserEnded,
+    #[msg("The amount to raise is below the minimum of 3 major units")]
+    InvalidAmount,
+    #[msg("Contributions have not all been refunded yet")]
+    RefundsOutstanding,
+    #[msg("Arithmetic overflow")]
+    MathOverflow,
+}
