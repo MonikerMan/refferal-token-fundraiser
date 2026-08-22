e8fc7c77f95db088f1c54eaa1133939036f8a5f3
Rox <11506193+MonikerMan@users.noreply.github.com>
2026-08-22 14:01:25 +0200

    Add Fundraiser struct with relevant fields

--- anchor/referal-fundraiser/src/state/fundraiser.rs
+++ anchor/referal-fundraiser/src/state/fundraiser.rs
@@ -1,1 +1,14 @@
+use anchor_lang::prelude::*;
 
+#[account]
+#[derive(InitSpace)]
+pub struct Fundraiser {
+    pub maker: Pubkey,
+    pub mint_to_raise: Pubkey,
+    pub amount_to_raise: u64,
+    pub current_amount: u64,
+    pub time_started: i64,
+    pub duration: u16,
+    pub is_paused: bool, // Track paused state
+    pub bump: u8,
+}
