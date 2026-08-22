e841edf39a526eb331a65400d0778023869cd358
Rox <11506193+MonikerMan@users.noreply.github.com>
2026-08-22 13:45:40 +0200

    Update mod.rs

--- anchor/referal-fundraiser/state/mod.rs
+++ anchor/referal-fundraiser/state/mod.rs
@@ -1,11 +1,15 @@
-pub mod contributor;
-pub mod fundraiser;
-pub mod referral_code_lookup;
-pub mod referred_contributor;
-pub mod referrer_profile;
+pub mod checker;
+pub mod close;
+pub mod contribute;
+pub mod contribute_referred;
+pub mod initialize_fundraiser;
+pub mod refund;
+pub mod register_referrer;
 
-pub use contributor::*;
-pub use fundraiser::*;
-pub use referral_code_lookup::*;
-pub use referred_contributor::*;
-pub use referrer_profile::*;
+pub use checker::*;
+pub use close::*;
+pub use contribute::*;
+pub use contribute_referred::*;
+pub use initialize_fundraiser::*;
+pub use refund::*;
+pub use register_referrer::*;
